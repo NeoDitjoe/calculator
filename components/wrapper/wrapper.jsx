c11f078b38ae721ff1b1aa2d7ca7ea7ba44f927f
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-01-30 06:25:50 +0200

    improved home page

--- components/wrapper/wrapper.jsx
+++ components/wrapper/wrapper.jsx
@@ -7,7 +7,7 @@ export default function Wrapper({Children}){
         <main>
             <nav className={style.nav}>
                 <Link href={'/'}></Link>
-                <Link href={'/calculatorV1'}>CalculatorV1</Link>
+                <Link href={'/'}>CalculatorV1</Link>
                 <Link href={'/calculatorV2'}>CalculatorV2</Link>
                 <Link href={'/calculatorV3'}>CalculatorV3</Link>
                 <Link href={'/calculatorV4'}>CalculatorV4</Link>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,47 @@
-export default function Okay(){
+import { useRef, useState } from 'react'
+import Link from 'next/link'
+
+export default function Home() {
+    const num1Ref = useRef()
+    const num2Ref = useRef()
+    const symbolRef = useRef()
+    const [results, setResults] = useState()
+  
+    function calculate(){
+      const num1 = num1Ref.current.value
+      const num2 = num2Ref.current.value
+      const symbol = symbolRef.current.value
+      if(num1.length > 0 && num2.length > 0){
+        if(symbol == '*'){
+          setResults(parseFloat(num1) * parseFloat(num2))
+        }
+        if(symbol == '-'){
+          setResults(parseFloat(num1) - parseFloat(num2))
+        }
+        if(symbol == '+'){
+          setResults(parseFloat(num1) + parseFloat(num2))
+        }
+        if(symbol == '/'){
+          setResults(parseFloat(num1) / parseFloat(num2))
+        }
+  
+      }else{
+        setResults("let's math")
+      }
+    } 
+  
     return (
-        <h1>Ohh Okay</h1>
+      <div>
+        <h1>Calculator</h1>
+  
+        <h2>{results}</h2>
+        <form onChange={calculate}>
+          <input type='number' ref={num1Ref}/>
+          <input type='text' ref={symbolRef}/>
+          <input type='number' ref={num2Ref}/>
+        </form> 
+        
+      </div>
     )
-}
+  }
+  
